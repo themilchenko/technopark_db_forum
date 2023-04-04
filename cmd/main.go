ade7c0fe28baa10801b89fdc8f89162ece6ebc96
themilchenko <ivan.milchenko.92@mail.ru>
2023-04-05 02:04:58 +0300

    add rps metric

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,27 @@ package main
 
 import (
         "technopark_db_forum/internal/app"
+        "github.com/labstack/echo-contrib/prometheus"
 
         "github.com/labstack/echo/v4"
 )
 
+const (
+        dbConfig = "host=84.201.157.36 port=5432 dbname=forum user=forum password=forum sslmode=disable"
+)
+
 func main() {
         e := echo.New()
-        s := app.New(e)
 
-        dbConfig := "host=84.201.157.36 port=5432 dbname=forum user=forum password=forum sslmode=disable"
+        // Create new echo instance for metrics
+        metricsWork := echo.New()
+        p := prometheus.NewPrometheus("echo", nil)
+
+        // Using middleware for defining wethher handler is works
+        e.Use(p.HandlerFunc)
+        p.SetMetricsPath(metricsWork)
+
+        s := app.New(e)
 
         if err := s.Start("0.0.0.0:8080", dbConfig); err != nil {
                 s.Echo.Logger.Error("server errors: %s", err)
