7f2b3678239d92469ddae0f944e2d1d4820bdd79
themilchenko <ivan.milchenko.92@mail.ru>
2023-04-04 05:22:50 +0300

    Change ip for container

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	"technopark_db_forum/internal/app"
+        "technopark_db_forum/internal/app"
 
-	"github.com/labstack/echo/v4"
-)
-
-const (
-	defaultDBConfig = "host=localhost port=5432 dbname=dev sslmode=disable"
+        "github.com/labstack/echo/v4"
 )
 
 func main() {
-	e := echo.New()
-	s := app.New(e)
+        e := echo.New()
+        s := app.New(e)
 
-	if err := s.Start("0.0.0.0:8080", defaultDBConfig); err != nil {
-		s.Echo.Logger.Error("server errors: %s", err)
-	}
+        dbConfig := "host=db port=5432 dbname=forum user=forum password=forum sslmode=disable"
+
+        if err := s.Start("0.0.0.0:8080", dbConfig); err != nil {
+                s.Echo.Logger.Error("server errors: %s", err)
+        }
 }
